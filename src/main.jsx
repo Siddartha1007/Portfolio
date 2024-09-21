0aa41b60681f9a0567f0bb478413f2851bd6ed43
Siddartha Goruganti <gsiddu1234@gmail.com>
2024-09-21 00:14:50 -0400

    Added browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename = "/Portfolio/">
+    <BrowserRouter basename = "/Portfolio/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
