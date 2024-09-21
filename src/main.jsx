7fc0ea81b9f1eef09fdac7abf0687a7bc3f4a83f
Siddartha Goruganti <gsiddu1234@gmail.com>
2024-09-21 00:10:27 -0400

    App

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename = "/Portfolio/">
-      <Route path = "*" element = {<App />} />
-      {/* <App /> */}
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
