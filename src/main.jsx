021d112992cfbfd12f3d4120fe157feb4b8bd443
Siddartha Goruganti <gsiddu1234@gmail.com>
2024-09-21 00:04:04 -0400

    Updated packages

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
     <BrowserRouter basename = "/Portfolio/">
       <App />
     </BrowserRouter>
-  // </StrictMode>,
+  </StrictMode>,
 );
